7806fea3e8a0fac175dc4be06b707b7d023df6a5
Endre Vegh <endre.vegh@yahoo.com>
2022-05-28 21:33:59 +0200

    [+]: add yabairc, skhd and wezterm configs

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+return {
+    keys = {{key = "=", mods = "CTRL", action = "IncreaseFontSize"}},
+    font_size = 16.0,
+    enable_tab_bar = false,
+    window_padding = {left = 0, right = 0, top = 0, bottom = 0},
+    enable_scroll_bar = false,
+    color_scheme = "Dracula",
+    -- No top but resizable for yabai.
+    window_decorations = "RESIZE"
+}
