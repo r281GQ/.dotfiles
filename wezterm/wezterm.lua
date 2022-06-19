c5aad036aceb063c4a2c805e6145791e8a85a259
Endre Vegh <endre.vegh@yahoo.com>
2022-06-19 12:29:14 +0200

    [~]: disable lalt for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,16 @@
 return {
     keys = {{key = "=", mods = "CTRL", action = "IncreaseFontSize"}},
     font_size = 16.0,
+    -- Hide tabs.
     enable_tab_bar = false,
-    window_padding = {left = 0, right = 0, top = 0, bottom = 0},
     enable_scroll_bar = false,
     color_scheme = "Dracula",
     -- No top but resizable for yabai.
-    window_decorations = "RESIZE"
+    window_decorations = "RESIZE",
+    -- Default folder to open.
+    cwd = '/Users/endrevegh/Repos',
+    window_padding = {left = 10, right = 0, top = 0, bottom = 0},
+    -- That makes wezterm not to consume lalt, so it can be used by yabai.
+    send_composed_key_when_left_alt_is_pressed = false,
+    send_composed_key_when_right_alt_is_pressed = true
 }
