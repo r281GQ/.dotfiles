6f5383e9c6d09311f9f5a7f64eedfded37f7ed11
Endre Vegh <endre.vegh@yahoo.com>
2022-06-27 17:11:21 +0200

    [~]: change diagnostics keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -844,10 +844,10 @@ vim.opt.foldlevel = 10
 local opts = {noremap = true, silent = true}
 vim.api.nvim_set_keymap('n', '<Leader>e',
                         '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
-                        opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>',
-                        opts)
+vim.api.nvim_set_keymap('n', '<leader>d[',
+                        '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>d]',
+                        '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>q',
                         '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
